091bfa244c9b11000c20865e4e4f17006ec893d1
fernand0labra <fernandolabracaso@gmail.com>
2024-04-05 13:33:54 +0200

    CORS Docker Update

--- packages/simulator/web/index.js
+++ packages/simulator/web/index.js
@@ -14,7 +14,7 @@ const server = require('http').createServer(ok)
 const io = require('socket.io')(server, 
   {
     cors: {
-      origin: 'http://localhost:3000', // 'http://localhost'
+      origin: 'http://localhost:3000',
       credentials: true,
       methods: ['GET', 'POST'],
     },
